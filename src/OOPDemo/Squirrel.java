7771e53577f062b9f38285f9825408c9dd899bb6
Matthew Yam <matthew.yam23@ycdsbk12.ca>
2022-10-19 10:44:01 -0400

    squirrel class done

---
+++ src/OOPDemo/Squirrel.java
@@ -0,0 +1,49 @@
+package OOPDemo;
+
+public class Squirrel{
+    //instance variables
+    String colourString;
+    String sizeString;
+    int nutCountInt;
+    boolean aliveBoolean;
+    double iqDouble;
+
+
+    //contructor
+
+    public Squirrel(String newColourString, String newSizeString, int newInt, int newNutCount, double newiqDouble){
+        this.aliveBoolean = true;
+        this.colourString = newColourString;
+        this.sizeString = newSizeString;
+        this.nutCountInt = newNutCount;
+        this.iqDouble = newiqDouble;
+    }
+
+    
+    //instance method
+
+    public void run(){
+        System.out.println("run!!");
+    }
+
+    public void findNut(int numberNuts){
+        nutCountInt = this.nutCountInt + numberNuts;
+    }
+
+    public int getNutCount(){
+        return this.nutCountInt;
+    }
+
+    public void eatNut(){
+        this.nutCountInt--;
+    }
+
+    public void climb(){
+        System.out.println("Climb that tree!!");
+    }
+
+    public void kill(){
+        this.aliveBoolean = false;
+    }
+
+}
---
+++ src/OOPDemo/SquirrelDemo.java
@@ -0,0 +1,4 @@
+package OOPDemo;
+public class SquirrelDemo {
+    
+}
